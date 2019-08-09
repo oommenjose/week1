c65795ff3c99a5709b9cfa2919db9d88ab3f7142
vasanthkashyap <vasanth.kashyap@niser.ac.in>
2019-08-09 14:36:18 +0530

    Completed Week1

---
+++ q1.sh
@@ -0,0 +1,6 @@
+echo What is your name?
+read name
+mkdir $name
+cd $name
+touch $name
+
---
+++ q2.sh
@@ -0,0 +1,8 @@
+echo What is your name?
+read name
+mkdir $name
+cd $name
+for i in 1 2 3 4 5
+do
+  touch $name""$i.cpp
+done
